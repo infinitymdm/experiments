59083a68c71687cab9171faec5b14b1e73477786
Marcus Mellor <infinitymdm@gmail.com>
2026-07-16 10:56:50 -0500

    liberty: Add sky130 custom characterize script

---
+++ liberty/sky130/characterize.py
@@ -0,0 +1,41 @@
+from charlib.characterizer.characterizer import Characterizer
+from charlib.cli import utils
+
+from tqdm import tqdm
+
+import memray
+
+from pathlib import Path
+import concurrent.futures
+import os
+
+def simulation_task_wrapper(task, *args):
+    with memray.Tracker(f'/tmp/memray-worker-{os.getpid()}.bin'):
+        result = task(*args)
+    return result
+
+def run(config_file):
+    config = utils.find_config(config_file)
+
+    characterizer = Characterizer(**config['settings'])
+    [characterizer.add_cell(n, p) for (n, p) in utils.read_cell_configs(config['cells'])]
+
+    # Custom characterize routine
+    for cell, config in characterizer.cells:
+        tasks = characterizer.analyse_cell(cell, config)
+        with tqdm(bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]', total=len(tasks), desc=cell.name) as progress_bar:
+            with concurrent.futures.ProcessPoolExecutor(max_workers=characterizer.settings.jobs) as executor:
+                futures = [executor.submit(simulation_task_wrapper, task) for task in tasks]
+                for completed in concurrent.futures.as_completed(futures):
+                    characterizer.library.add_group(completed.result())
+                    progress_bar.update(1)
+
+    # Add LUTs
+    for timing_group in characterizer.library.subgroups_with_name('timing'):
+        for lut_group in timing_group.groups.values():
+            characterizer.library.add_group(lut_group.template)
+
+    print(characterizer.library.to_liberty(precision=6))
+
+if __name__ == "__main__":
+    run('sky130_fd_sc_hd__tt_025C_1v80.yaml')
