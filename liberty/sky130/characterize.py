0f5179c770df5430f79619f4bef34d7f14bcf6d8
Marcus Mellor <infinitymdm@gmail.com>
2026-07-16 13:52:39 -0500

    Use random strings for per-thread profiling

--- liberty/sky130/characterize.py
+++ liberty/sky130/characterize.py
@@ -7,10 +7,13 @@ import memray
 
 from pathlib import Path
 import concurrent.futures
+import random
+import string
 import os
 
 def simulation_task_wrapper(task, *args):
-    with memray.Tracker(f'/tmp/memray-worker-{os.getpid()}.bin'):
+    task_id = ''.join(random.choices(string.ascii_letters, k=8))
+    with memray.Tracker(f'tmp/memray-task-{task.__name__}-{task_id}.bin'):
         result = task(*args)
     return result
 
@@ -18,14 +21,19 @@ def run(config_file):
     config = utils.find_config(config_file)
 
     characterizer = Characterizer(**config['settings'])
-    [characterizer.add_cell(n, p) for (n, p) in utils.read_cell_configs(config['cells'])]
+    for name, config in utils.read_cell_configs(config['cells']):
+        try:
+            characterizer.add_cell(name, config)
+        except ValueError as e:
+            print(e)
+            continue
 
-    # Custom characterize routine
+    # Custom characterize routine - characterize one cell at a time
     for cell, config in characterizer.cells:
         tasks = characterizer.analyse_cell(cell, config)
         with tqdm(bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]', total=len(tasks), desc=cell.name) as progress_bar:
             with concurrent.futures.ProcessPoolExecutor(max_workers=characterizer.settings.jobs) as executor:
-                futures = [executor.submit(simulation_task_wrapper, task) for task in tasks]
+                futures = [executor.submit(simulation_task_wrapper, task, *args) for (task, *args) in tasks]
                 for completed in concurrent.futures.as_completed(futures):
                     characterizer.library.add_group(completed.result())
                     progress_bar.update(1)
